7e81f53957f08e1f9965f432cae0b8c67df87c06
ahmed <ahmedadam.boussarsar@gmail.com>
2023-11-06 00:35:16 +0100

    Commit tp3

--- app/src/main/java/com/example/mesure_glycemie/MainActivity.java
+++ app/src/main/java/com/example/mesure_glycemie/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //Question1
@@ -20,54 +21,128 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton BtnNon;
     private EditText ETValMes;
     private Button BtnConsulter;
+    private TextView text;
 
     //Question 2
-    private void init()
-    {
+    private void init() {
         TVAge = findViewById(R.id.TVAge);
         sbAge = findViewById(R.id.sbAge);
         BtnOui = findViewById(R.id.BtnOui);
         BtnNon = findViewById(R.id.BtnNon);
         ETValMes = findViewById(R.id.ETValMes);
         BtnConsulter = findViewById(R.id.BtnConsulter);
+        text = findViewById(R.id.text);
+        //Question 3
+        sbAge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("Information", "onProgressChanged " + progress);
+                // affichage dans le Log de Android studio pour voir les changements détectés sur le SeekBar ..
+                TVAge.setText("Votre âge : " + progress);
+                // Mise à jour du TextView par la valeur: progress à chaque changement.
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        BtnConsulter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int age = sbAge.getProgress();
+                System.out.println("age =" + age);
+                String valmesureSTR = (ETValMes.getText().toString());
+                System.out.println("val =" + valmesureSTR);
+                if (age == 0 && valmesureSTR.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Age and valeur mesure invalide", Toast.LENGTH_SHORT).show();
+                } else if (age == 0) {
+                    Toast.makeText(getApplicationContext(), "age invalide", Toast.LENGTH_SHORT).show();
+                } else if (valmesureSTR.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), " valeur mesure invalide", Toast.LENGTH_SHORT).show();
+                } else {
+                    float valmesure = Float.parseFloat(valmesureSTR);
+                    boolean isFasting = BtnOui.isChecked();
+                    System.out.println("is the user fasting? = " + isFasting);
+                    if (isFasting) {
+                        if (age > 13) {
+                            if (valmesure >= 5.0 && valmesure <= 7.2) {
+                                System.out.println("Niveau de glycémie est normale");
+                                text.setText("Niveau de glycémie est normale");
+                            } else if (valmesure < 5.0) {
+
+                                System.out.println("Niveau de glycémie est trop bas");
+                                text.setText("Niveau de glycémie est trop bas");
+                            } else if (valmesure > 7.2) {
+                                System.out.println("Niveau de glycémie est trop élevée");
+                                text.setText("Niveau de glycémie est trop élevée");
+                            }
+                        } else if (age >= 6 && age <= 12) {
+                            if (valmesure >= 5.0 && valmesure <= 10.0) {
+                                System.out.println("Niveau de glycémie est normale");
+                                text.setText("Niveau de glycémie est normale");
+                            } else {
+                                System.out.println("Niveau de glycémie est trop bas");
+                                text.setText("Niveau de glycémie est trop bas");
+                            }
+                        } else if (age < 6) {
+                            if (valmesure >= 5.5 && valmesure <= 10.0) {
+                                System.out.println("Niveau de glycémie est normale");
+                                text.setText("Niveau de glycémie est normale");
+                            } else {
+                                System.out.println("Niveau de glycémie est trop bas");
+                                text.setText("Niveau de glycémie est trop bas");
+                            }
+                        }
+                    } else {
+                        if (valmesure < 10.5) {
+                            System.out.println("Niveau de glycémie est normale");
+                            text.setText("Niveau de glycémie est normale");
+                        } else {
+                            System.out.println("Niveau de glycémie est trop élevée");
+                            text.setText("Niveau de glycémie est trop élevée");
+                        }
+                    }
+                }
+            }
+        });
     }
-    //Question 4 (Méthode Implicite)
-    public void calculer(View view)
-    {
-        // Ajoutez votre code ici pour gérer l'événement de clic sur le bouton Consulter
-        // Cette méthode sera déclenchée lorsque le bouton est cliqué
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        //Question 3
-        sbAge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-            {
-                Log.i("Information", "onProgressChanged "+progress);
-                // affichage dans le Log de Android studio pour voir les changements détectés sur le SeekBar ..
-                TVAge.setText("Votre âge : "+ progress);
-                // Mise à jour du TextView par la valeur: progress à chaque changement.
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-        /*Question 5 (Méthode Excplicite)
-        BtnConsulter.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                // Ajoutez votre code ici pour gérer l'événement de clic sur le bouton Consulter
-                // Cette méthode sera déclenchée lorsque le bouton est cliqué
-            }
-        });
-         */
     }
 }
+       /*Question 4 (Méthode Implicite)
+        public void calculer(View view) {
+        // Obtenez la valeur de la glycémie entrée par l'utilisateur
+        EditText ETValMes = findViewById(R.id.ETValMes);
+        double valeurGlycemie = Double.parseDouble(ETValMes.getText().toString());
+        // Vérifiez si le patient à jeuner ou non
+        boolean estJeun = true;
+        // Vérifiez les intervalles en fonction de l'âge
+        if (estJeun) {
+            if (valeurGlycemie >= 5.0 && valeurGlycemie <= 7.2) {
+                Toast.maketext(getApplicationContext(), "Niveau de glycémie est normal").show();
+            } else if (valeurGlycemie < 5.0) {
+                Toast.maketext(getApplicationContext(), "Niveau de glycémie est trop bas").show();
+            } else {
+                Toast.maketext(getApplicationContext(), "Niveau de glycémie est trop élevée").show();
+            }
+        }
+        else {
+            if (valeurGlycemie < 10.5) {
+                Toast.maketext(getApplicationContext(), "Niveau de glycémie est normal").show();
+            } else {
+                Toast.maketext(getApplicationContext(), "Niveau de glycémie est trop élevée").show();
+            }
+        }
+    }
+    */
